cc66e6f341912391511642ffd19e3125f2754cf5
ozetin <vadimkairov20@gmail.com>
2025-11-12 12:15:48 +0300

    Update zadanie1_List_comprehensions.py

--- Задания/zadanie1_List_comprehensions.py
+++ Задания/zadanie1_List_comprehensions.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     poisk = input("Введите элемент: ")
     naiden = [i for i, elem in enumerate(spisok) if elem == poisk]
 
-    if len(naiden) == 0:
-        print("Заданного элемента в списке нет", file=sys.stderr)
-    else:
+    if naiden:
         print(naiden)
+    else:
+        print("Заданного элемента в списке нет", file=sys.stderr)
